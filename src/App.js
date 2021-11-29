d8f90bd61d40deb848b3eb0387832572839fd66c
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-28 20:55:35 -0500

    created on submit function of adding funds and staking to lottery;

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends React.Component {
     manager: '',
     players: [],
     balance: '',
-    value: ''
+    value: '',
+    message: ''
   };
 
   async componentDidMount() {
@@ -17,7 +18,22 @@ class App extends React.Component {
     const players = await lottery.methods.getPlayers().call();
     const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager, players });
+    this.setState({ manager, players, balance });
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+    
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction success...' });
+
+    await lottery.methods.enter().send({ 
+      from: accounts[0], 
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+    this.setState({ message: 'You have been entered!' });
   }
 
   render() {
@@ -30,7 +46,7 @@ class App extends React.Component {
           competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
         <hr />
-        <form>
+        <form onSubmit={this.onSubmit}>
           <h4>Want to try your luck?</h4>
           <div>
             <label>Amount of ether to enter</label>
@@ -41,6 +57,15 @@ class App extends React.Component {
           </div>
           <button>Enter</button>
         </form>
+
+        <hr />
+
+        <h4>Ready to pick a winner?</h4>
+        <button onClick={this.onClick}>Pick a winner</button>
+
+        <hr />
+        
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
